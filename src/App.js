8461be1241d791f37395703e9f63b9402f0a9193
maryojo <maryojo3@gmail.com>
2022-07-17 15:00:02 +0100

    Debugged routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
-import Pages from "./pages";
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/home';
+import Destination from "./pages/destination";
+import Crew from "./pages/crew";
+import Tech from "./pages/tech";
+import Explore from "./pages/explore";
 
 function App() {
   return (
-    <div className="App">
-    <Pages />
-    </div>
+    <BrowserRouter>
+    <Routes>
+     <Route path="/" element={<Home />} />
+     <Route path="/destination" element={<Destination />} />
+     <Route path="/crew" element={<Crew />} />
+     <Route path="/technology" element={<Tech />} />
+     <Route path="/explore" element={<Explore />} />
+   </Routes>
+</BrowserRouter>
   );
 }
 
