81115417ec4cd873de61c16b2ccc86a9e07d389f
maryojo <maryojo3@gmail.com>
2022-08-03 20:10:28 +0100

    Debugging

--- src/components/NasaImage.js
+++ src/components/NasaImage.js
@@ -1,35 +1,46 @@
 import React, {useState, useEffect } from 'react'
-import axios from 'axios'
-import requests from '../api/requests';
+// import axios from 'axios'
+// import requests from '../api/requests';
 
 const NasaImage = () => {
-  const fetchURL = requests.imageOfDay;
+  // const fetchURL = requests.imageOfDay;
+  useEffect(() => {
+    getImage();
+  }, []);
+
   const [image, setImage] = useState();
 
-  try{
-    useEffect(() => {
-      return () => {
-        axios.get(fetchURL).then((response) =>{
-          setImage(response.data);
-         // setImage(response.data.results);
-        })
-      };
-    }, [fetchURL]);
-  
-    console.log(image);
-  } catch(error) {
-      if(error.response){
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-    } else if (error.request) {
-        // The client never received a response, and the request was never left
-        console.log("I received");
-    } else {
-        // Anything else
-        console.log("Sth else")
-      }
+  const getImage = async () =>{
+      const api = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_APIKEY}`);
+      const data = await api.json();
+      console.log(data);
+      setImage(data)
   }
+
+  // try{
+  //   useEffect(() => {
+  //     return () => {
+  //       axios.get(fetchURL).then((response) =>{
+  //         setImage(response.data);
+  //        // setImage(response.data.results);
+  //       })
+  //     };
+  //   }, [fetchURL]);
+  
+  //   console.log(image);
+  // } catch(error) {
+  //     if(error.response){
+  //       console.log(error.response.data);
+  //       console.log(error.response.status);
+  //       console.log(error.response.headers);
+  //   } else if (error.request) {
+  //       // The client never received a response, and the request was never left
+  //       console.log("I received");
+  //   } else {
+  //       // Anything else
+  //       console.log("Sth else")
+  //     }
+  // }
   
   return (
     <div className='p-20 backdrop-blur-sm bg-white/20 rounded-sm '>
